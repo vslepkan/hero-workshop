66a91d83f58b5c513493715926d5e4c521e40ded
Vladyslav Slepkan <vslepkan@gmail.com>
2020-01-16 12:30:34 +0200

    remove unused import

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Card from "components/Card/Card";
 import Search from "components/Search/Search";
